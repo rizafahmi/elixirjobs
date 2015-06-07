d0bb0b9cd1df938e22b96ddabb69370954ff056c
Riza Fahmi <rizafahmi@gmail.com>
2015-06-07 08:24:27 +0700

    Add exrethinkdb to the dependencies

--- mix.exs
+++ mix.exs
@@ -31,6 +31,8 @@ defmodule ElixirJobs.Mixfile do
     [{:phoenix, "~> 0.13.1"},
      {:phoenix_html, "~> 1.0"},
      {:phoenix_live_reload, "~> 0.4", only: :dev},
-     {:cowboy, "~> 1.0"}]
+     {:cowboy, "~> 1.0"},
+     {:exrethinkdb, github: "hamiltop/exrethinkdb", ref: "55fb5b5ed892f28b7ae8ee1b2f8e54fb651bd611"},
+    ]
   end
 end
