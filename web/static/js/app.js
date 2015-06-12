f94fb117105de64eac309f04c174af4d4ae4f5c5
Riza Fahmi <rizafahmi@gmail.com>
2015-06-12 07:51:16 +0700

    Add a simple form validation

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -12,4 +12,33 @@ let App = {
 
 $('.ui.checkbox').checkbox();
 
+var formSettings = {inline : true, on : 'blur'};
+
+var formRules = {
+  name : {
+    identifier : 'name',
+    rules : [{
+      type : 'empty',
+      prompt : 'Please enter a name'
+    }]
+  },
+  password : {
+    identifier : 'password',
+    rules : [{
+      type : 'empty',
+      prompt : 'Please enter your super secret password'
+    }]
+  },
+  confirmPassword : {
+    identifier : 'confirm_password',
+    rules : [{
+      type: 'match[password]',
+      prompt : 'Password should be the same'
+    }]
+  }
+};
+
+
+$(".ui.form").form(formRules, formSettings);
+
 export default App
