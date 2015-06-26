6394a7c556310fca2ac8e9451c9bab84f929f320
Riza Fahmi <rizafahmi@gmail.com>
2015-06-27 00:13:06 +0700

    Change header style

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -21,7 +21,7 @@ defmodule ElixirJobs.PageController do
       |> Query.filter(%{email: get_session(conn, :user)})
       |> Repo.run
 
-    if get_session(conn, :user) do
+    if get_session(conn, :user) && List.first(current_dev.data) do
       current_dev = List.first(current_dev.data)
     else
       current_dev = nil
