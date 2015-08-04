85e3d1ae31716ae31c749f95f6f9ac8567b11656
optikfluffel <optik@fluffel.io>
2015-08-04 15:48:58 +0200

    cleanup routes and add /feed

--- web/router.ex
+++ web/router.ex
@@ -8,34 +8,34 @@ defmodule ElixirJobs.Router do
     plug :protect_from_forgery
   end
 
-  pipeline :api do
-    plug :accepts, ["json"]
+  pipeline :rss do
+    plug :accepts, ["rss"]
   end
 
   scope "/", ElixirJobs do
     pipe_through :browser # Use the default browser stack
 
-    # resources "/", PageController
-    get "/", PageController, :index
-    get "/job/new", PageController, :new
-    get "/job/:id", PageController, :show
+    get  "/", PageController, :index
+    get  "/job/new", PageController, :new
+    get  "/job/:id", PageController, :show
     post "/job", PageController, :create
 
-    get "/users/login", UserController, :login
+    get  "/users/login", UserController, :login
     post "/users/process_login", UserController, :process_login
+    get  "/users/logout", UserController, :logout
+
     post "/users", UserController, :create
-    get "/users/new_profile", UserController, :new_profile
-    get "/users/edit_profile", UserController, :edit_profile
+    get  "/users/new_profile", UserController, :new_profile
+    get  "/users/edit_profile", UserController, :edit_profile
     post "/users/profile", UserController, :create_profile
     post "/users/edit_profile", UserController, :update_profile
-    get "/users/logout", UserController, :logout
 
-    get "/dev/:id", DevController, :show
-
+    get  "/dev/:id", DevController, :show
   end
 
-  # Other scopes may use custom stacks.
-  # scope "/api", ElixirJobs do
-  #   pipe_through :api
-  # end
+  scope "/feed", ElixirJobs do
+    pipe_through :rss # Use the rss stack
+
+    get "/", FeedController, :index
+  end
 end
