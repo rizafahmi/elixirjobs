37834058e5d1592ef617ed91ac639bc425228188
optikfluffel <optik@fluffel.io>
2015-08-04 12:37:11 +0200

    use timex to show human-readable date

--- web/views/page_view.ex
+++ web/views/page_view.ex
@@ -1,8 +1,10 @@
 defmodule ElixirJobs.PageView do
   use ElixirJobs.Web, :view
+  use Timex
 
   def convert_date(epoch_time) do
-    epoch_time
+    epoch_time |> Date.from(:secs, :epoch)
+               |> DateFormat.format!("%Y-%m-%d", :strftime)
   end
 
   def to_html(text) do
