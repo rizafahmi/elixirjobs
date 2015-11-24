5621acef3daa4ee33a3259643d3c434a7594e411
Manu S Ajith <neo@codingarena.in>
2015-11-25 00:49:46 +0530

    Update dependencies

--- mix.exs
+++ mix.exs
@@ -28,7 +28,7 @@ defmodule ElixirJobs.Mixfile do
   #
   # Type `mix help deps` for examples and options
   defp deps do
-    [{:phoenix, "~> 0.17"},
+    [{:phoenix, "~> 1.0.3"},
      {:phoenix_html, "~> 2.0"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:cowboy, "~> 1.0"},
